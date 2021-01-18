b31133d0f56dcf868cb5b3c736ea490eb9d30b5f
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-18 22:37:39 +0000

    Updated templates section checking the login and sign up features

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+development = os.environ.get('DEVELOPMENT', True)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
